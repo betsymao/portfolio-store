019db2c8992761c62a2f153f8a8983a1601550c5
Betsy <132311670+betsymao@users.noreply.github.com>
2023-12-13 17:06:40 +1100

    route nesting and updates

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,19 @@ import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
 import Error from './pages/Error';
 
+// Components
+import Layout from './components/layout/Layout';
+
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/product/:id' element={<ProductDetails />} />
-        <Route path='*' element={<Error />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetails />} />
+          <Route path='*' element={<Error />} />
+        </Route>
       </Routes>
     </>
   );
