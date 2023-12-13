e4ea7103459e9283c2a0c37488722eb0b9356250
Betsy <132311670+betsymao@users.noreply.github.com>
2023-12-13 14:58:03 +1100

    set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+// React Router Dom
+import { Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+import Error from './pages/Error';
+
 function App() {
 
   return (
     <>
-    
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/product/:id' element={<ProductDetails />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
     </>
   );
 }
