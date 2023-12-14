b7a6981e58ab427aa70cfd290a5fb8aecacbc265
Betsy <132311670+betsymao@users.noreply.github.com>
2023-12-14 20:23:30 +1100

    product menu pages

---
+++ src/pages/ProductWomens.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+
+// Product Context
+import ProductContext from '../contexts/ProductContext.jsx';
+
+// Components
+import Product from '../components/features/Product.jsx'
+
+function ProductWomens() {
+    const { products } = useContext(ProductContext);
+    // console.log(products);
+  
+    // get category women's clothing
+    const filteredProducts = products.filter(item => {
+      return item.category === `women's clothing`;
+    });
+    // console.log(filteredProducts);
+  
+    return (
+      <>
+        {filteredProducts.map(product => {
+          return (
+            <Product key={product.id} product={product} />
+          );
+        })}
+      </>
+    );
+}
+
+export default ProductWomens;
