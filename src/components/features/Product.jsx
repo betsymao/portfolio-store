9dd93d03728ac050925c2fcf8af466f66ed96536
Betsy <132311670+betsymao@users.noreply.github.com>
2023-12-14 20:25:13 +1100

    product component

--- src/components/features/Product.jsx
+++ src/components/features/Product.jsx
@@ -1,7 +1,20 @@
-function Product() {
+import { useContext } from 'react';
+
+import { Link } from 'react-router-dom';
+
+function Product({ product}) {
+  // console.log(product);
+  const { id, image, title, price } = product;
+
     return (
       <>
-        
+        <Link to={`/product/${id}`}>
+          <img src={image} alt='' />
+        </Link>
+        <Link to={`/product/${id}`}>
+          <div>{title}</div>
+        </Link>
+        <div>${price}</div>
       </>
     );
   }
