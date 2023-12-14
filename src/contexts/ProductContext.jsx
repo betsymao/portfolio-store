7ad051efef70930dca5a2630337f44069273b19e
Betsy <132311670+betsymao@users.noreply.github.com>
2023-12-14 18:39:51 +1100

    update product context

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -2,21 +2,20 @@ import { createContext, useState, useEffect } from 'react';
 
 const ProductContext = createContext();
 
-export function ProductProvider({ children }) {
+export function ProductProvider ({ children }) {
     const [products, setProducts] = useState([]);
     
     useEffect(() => {
         const fetchProducts = async () => {
             const res = await fetch('https://fakestoreapi.com/products');
             const data = await res.json();
-            console.log(data);
             setProducts(data);
         };
         fetchProducts();
-    });
+    }, []);
 
     return (
-        <ProductContext.Provider>
+        <ProductContext.Provider value={{ products }}>
             {children}
         </ProductContext.Provider>
     );
