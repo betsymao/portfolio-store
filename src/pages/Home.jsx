498c4919bb7bd4c9416a44b4e2e6d582d4a47b92
Betsy <132311670+betsymao@users.noreply.github.com>
2023-12-16 12:49:45 +1100

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,29 @@ import Col from 'react-bootstrap/Col';
 function Home() {
   return (
     <>
-      <Row>
-        <Col md="6" lg="6">
+      <Row className="g-0">
+        <Col md="6" lg="6" className="p-0">
           <div>
-            IMG 1
+            <img
+              srcSet="../../public/assets/hero-men-s.jpg 320w,
+                      ../../public/assets/hero-men-w.jpg 600w,
+                      ../../public/assets/hero-men-l.jpg 1200w,
+                      ../../public/assets/hero-men-xl.jpg 1920w"
+              sizes="(min-width: 1200px) 1200px, 100vw"
+              src="../../public/assets/hero-men.jpg"
+              alt="img alt" />
           </div>
         </Col>
-        <Col md="6" lg="6">
+        <Col md="6" lg="6" className="p-0">
           <div>
-            IMG 2
+            <img
+              srcSet="../../public/assets/hero-women-s.jpg 320w,
+                      ../../public/assets/hero-women-w.jpg 600w,
+                      ../../public/assets/hero-women-l.jpg 1200w,
+                      ../../public/assets/hero-women-xl.jpg 1920w"
+                sizes="(min-width: 1200px) 1200px, 100vw"
+                src="../../public/assets/hero-women.jpg"
+                alt="img alt" />
           </div>
         </Col>
       </Row>
