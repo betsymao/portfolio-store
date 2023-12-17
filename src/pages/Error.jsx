6c04129f3014fab86a2b878de5b69759555a0c25
Betsy <132311670+betsymao@users.noreply.github.com>
2023-12-17 16:53:43 +1100

    updated error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
+
+// React Bootstrap Components
+import Container from 'react-bootstrap/Container';
+
 function Error() {
     return (
       <>
-      
+        <Container className="pt-5">
+          <h2>Page not found</h2>
+          <p>Sorry, we can't seem to find the page you're looking for.</p>
+          <Link>Back to home</Link>
+        </Container>
       </>
     );
   }
