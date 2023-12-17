af5484fb609b25efaf99804bc2565f741d21afd9
Betsy <132311670+betsymao@users.noreply.github.com>
2023-12-17 20:56:07 +1100

    fixed img srcset paths

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 // React Bootstrap Components
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -9,25 +11,27 @@ function Home() {
         <Col md="6" lg="6" className="p-0">
           <div>
             <img
-              srcSet="../../public/assets/hero-men-s.jpg 320w,
-                      ../../public/assets/hero-men-w.jpg 600w,
-                      ../../public/assets/hero-men-l.jpg 1200w,
-                      ../../public/assets/hero-men-xl.jpg 1920w"
+              srcSet="/src/assets/hero-men-s.jpg 320w,
+                      /src/assets/hero-men-w.jpg 600w,
+                      /src/assets/hero-men-l.jpg 1200w,
+                      /src/assets/hero-men-xl.jpg 1920w"
               sizes="(min-width: 1200px) 1200px, 100vw"
-              src="../../public/assets/hero-men.jpg"
+              src="/src/assets/hero-men.jpg"
               alt="img alt" />
+              <Link to="/men">Men</Link>
           </div>
         </Col>
         <Col md="6" lg="6" className="p-0">
           <div>
             <img
-              srcSet="../../public/assets/hero-women-s.jpg 320w,
-                      ../../public/assets/hero-women-w.jpg 600w,
-                      ../../public/assets/hero-women-l.jpg 1200w,
-                      ../../public/assets/hero-women-xl.jpg 1920w"
+              srcSet="/src/assets/hero-women-s.jpg 320w,
+                      /src/assets/hero-women-w.jpg 600w,
+                      /src/assets/hero-women-l.jpg 1200w,
+                      /src/assets/hero-women-xl.jpg 1920w"
                 sizes="(min-width: 1200px) 1200px, 100vw"
-                src="../../public/assets/hero-women.jpg"
+                src="/src/assets/hero-women.jpg"
                 alt="img alt" />
+              <Link to="/women">Women</Link>
           </div>
         </Col>
       </Row>
