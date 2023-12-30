0eb9d14d83efcbb08bae68dfe72ee6d94c44e942
Betsy <132311670+betsymao@users.noreply.github.com>
2023-12-30 15:38:42 +1100

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
+// React Bootstrap
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 // React Router Dom
 import { BrowserRouter } from 'react-router-dom';
 
 // Product Provider
 import { ProductProvider } from './contexts/ProductContext.jsx';
 
-// React Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
+// Cart Provider
+import { CartProvider } from './contexts/CartContext.jsx';
+
+// Stylesheet
+import './css/main.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ProductProvider>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </ProductProvider>
+  <CartProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </ProductProvider>
+  </CartProvider>
 );
