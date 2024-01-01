7b2be50b6b2625efe7ac4638ad7122869578513f
Betsy <132311670+betsymao@users.noreply.github.com>
2024-01-01 21:06:29 +1100

    updated hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,30 +9,40 @@ function Home() {
     <>
       <Row className="g-0">
         <Col md="6" lg="6" className="p-0">
-          <div>
-            <Link to="/men">
-              <picture>
-                <source media="(min-width: 1200px)" 
-                      srcSet="/src/assets/hero-men-xl.jpg" />
-                <img
-                  src="/src/assets/hero-men-l.jpg"
-                  alt="Image link to men's clothing" />
-              </picture>
-            </Link>
-          </div>
+          <Link to="/men">
+            <div className="hero-img-container">
+                <picture>
+                  <source media="(min-width: 1200px)" 
+                        srcSet="/src/assets/hero-men-xl.jpg" />
+                  <img
+                    src="/src/assets/hero-men-l.jpg"
+                    alt="Image link to men's clothing" />
+                </picture>
+              <div className="overlay">
+                <div className="overlay-content">
+                  <p className="hero-text">Men</p>
+                </div>
+              </div>
+            </div>
+          </Link>
         </Col>
         <Col md="6" lg="6" className="p-0">
-          <div>
-            <Link to="/women">
-              <picture>
-                <source media="(min-width: 1200px)"
-                        srcSet="/src/assets/hero-women-xl.jpg" />
-                <img
-                  src="/src/assets/hero-women-l.jpg"
-                  alt="Image link to women's clothing" />
-              </picture>
-              </Link>
-          </div>
+          <Link to="/women">
+            <div className="hero-img-container">
+                <picture>
+                    <source media="(min-width: 1200px)"
+                            srcSet="/src/assets/hero-women-xl.jpg" />
+                    <img
+                      src="/src/assets/hero-women-l.jpg"
+                      alt="Image link to women's clothing" />
+                  </picture>
+              <div className="overlay">
+                <div className="overlay-content">
+                  <p className="hero-text">Women</p>
+                </div>
+              </div>
+            </div>
+          </Link>
         </Col>
       </Row>
     </>
