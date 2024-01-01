9fe82a567ab3006232c40cd6111deb155557412a
Betsy <132311670+betsymao@users.noreply.github.com>
2024-01-01 21:05:56 +1100

    updated header component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 
 // React Bootstrap Components
 import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav';
 
+import Hamburger from './../../assets/hamburger.svg';
+import Close from './../../assets/close.svg';
 // Cart Context
 import CartContext from '../../contexts/CartContext';
 
@@ -15,50 +17,44 @@ import Logo from '../../assets/logo.svg';
 function Header() {
   const { quantity } = useContext(CartContext);
 
+
+
+
     return (
       <>
         <header>
           <Container>
-            <Navbar collapseOnSelect expand="sm" className="justify-content-between">
-              <div className="header-menu">
-                <Navbar.Toggle />
-                <Navbar.Collapse>
-                  <Nav>
-                    <li className="header-nav-list-item">
-                      <Link to='/men'>
+            <div className="nav-bar">
+              <div>                
+                <Link className="p-0" to="/">
+                  <img className="nav-brand" src={Logo} alt="Okapi Logo" />
+                </Link>
+              </div>
+
+              <nav>
+                <ul className="nav-menu">
+                    <li className="nav-item">
+                      <Link to="/men">
                         Men
                       </Link>
                     </li>
-                    <li className="header-nav-list-item">
-                      <Link to='/women'>
+                    <li className="nav-item">
+                      <Link to="/women">
                         Women
                       </Link>
                     </li>
-                  </Nav>
-                </Navbar.Collapse>  
-              </div>            
+                </ul>
+              </nav>
 
-              <Navbar.Brand className="m-0">
-                <div className="logo">
-                  <Link to='/'>
-                    <img src={Logo} />
-                  </Link>
-                </div>
-              </Navbar.Brand>
-
-              <div className="cart justify-self-end d-flex">
-                <Nav>
-                    <li className="cart-list-item">
-                      <Link to='/cart'>
-                        Cart
-                      </Link>
-                    </li>
-                    <li className="cart-list-item">
-                    &nbsp;({quantity})
-                    </li>
-                  </Nav>
+              <div className="nav-cart">
+                <Link to="/cart">
+                  Cart
+                </Link>
+                <span>
+                  &nbsp;({quantity})
+                </span>
               </div>
-            </Navbar>
+            </div>
           </Container>
         </header>
       </>
